46451bf73c35836526d3c52213b61bca6dc78371
camshaft <bytheway.cameron@gmail.com>
2015-12-22 16:05:53 -0700

    release 0.2.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MazurkaMediatypeHyperjson.Mixfile do
 
   def project do
     [app: :mazurka_mediatype_hyperjson,
-     version: "0.2.1",
+     version: "0.2.2",
      elixir: "~> 1.0",
      description: "hyper+json mediatype compiler for mazurka",
      package: package,
